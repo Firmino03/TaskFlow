e1b1e3ea107ed269e2777f661cd23987b2d3e7b7
Firmino <karolaynefirmino27@gmail.com>
2026-07-30 20:55:44 -0300

    feat: criação de AuthGuard e HTTP Interceptor"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login';
 import { RegisterComponent } from './features/auth/register/register';
+import { authGuard } from './core/guards/auth-guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+
+  { path: 'projects', canActivate: [authGuard], component: LoginComponent },
 ];
